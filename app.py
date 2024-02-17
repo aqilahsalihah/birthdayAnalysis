ff0f2986b9c1af669fbd369ed1220aaa98591c9b
Aqilah Salihah <aqilahsalihah02@gmail.com>
2024-02-17 13:13:58 +0800

    add requirements

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title='Birthday Annalysis', page_icon=':birthday_cake:', layout='wide')
-
 st.title("How Rare is Your Birthday")
